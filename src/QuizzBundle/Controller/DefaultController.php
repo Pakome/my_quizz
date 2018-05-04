9798ccffb40a7d34f4cac6d8ef75a0e5c7466364
Fleisch Frédéric <frederic.fleisch@epitech.eu>
2018-05-04 14:04:43 +0200

    View index Bundle

--- src/QuizzBundle/Controller/DefaultController.php
+++ src/QuizzBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('QuizzBundle:Default:index.html.twig');
+        return $this->render('@Quizz/Default/index.html.twig');
     }
 }
